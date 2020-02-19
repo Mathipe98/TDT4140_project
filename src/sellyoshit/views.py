fd7a85e4ecb4d1e5366475688d1802f32ce61c50
alfhusem <alfbhusem@gmail.com>
2020-02-19 12:34:59 +0100

    Added gridview for product list

--- src/sellyoshit/views.py
+++ src/sellyoshit/views.py
@@ -7,10 +7,10 @@ def products(request):
     context = {'products': products}
     return render(request, 'sellyoshit/product_list.html', context)
 
-def product(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {'product': product}
-    return render(request, 'sellyoshit/product_list.html', context)
+def ad(request, pk):
+    ad = Product.objects.get(pk=pk)
+    context = {'ad': ad}
+    return render(request, 'sellyoshit/product_details.html', context)
 
 def home(request):
     return render(request, 'sellyoshit/home_page2.html')
@@ -18,3 +18,4 @@ def home(request):
 def image_test(request):
     #index_file_path = PROJECT_PATH + '/templates/sellyoshit/Clickable_image_test.html'
     return render(request, "sellyoshit/Clickable_image_test.html")
+
