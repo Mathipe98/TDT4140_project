ca7d54dfe3d00b11eed7c5123b32941a08045ab1
Thor-Herman <thorherman.eggelen@gmail.com>
2020-02-21 14:01:19 +0100

    Updated advertisement and ad_view

--- src/ads/views.py
+++ src/ads/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.contrib.auth.models import User
 from .models import Advertisement
 # Create your views here.
 
