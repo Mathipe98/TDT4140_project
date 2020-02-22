ccb0a6f0b69b40da985ea6aef33c2737522c3235
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-02-22 17:25:28 +0100

    Added edit page and changed thanks

--- src/ads/urls.py
+++ src/ads/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('<int:pk>/', views.show_specific_ad, name='specific_ad'),
     # <int:pk> means that django expects an int (pk) and will transfer
     # it to the view as such. Transfers pk = int to show_specific_ad
-    path('ads/', views.ad_detail_view, name='fetch_add'),
-    path('new/', views.create_ad, name='create_add'),
-    path('thanks/', views.response, name='thanks')
-
+    path('ads/', views.ad_detail_view, name='fetch_ad'),
+    path('new/', views.create_ad, name='create_ad'),
+    path('thanks/', views.thanks_response, name='thanks_response'),
+    path('edit/<int:pk>/', views.edit_ad, name='edit_ad')
 ]
 
