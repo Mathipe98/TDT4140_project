8294796c54f3f8b9de7ca21559e887f112f85a58
Thor-Herman <thorherman.eggelen@gmail.com>
2020-03-12 09:53:51 +0100

    Created necessary user files for profile
    Removed redundant advertisement links in urls.py
    Created files for viewing a user profile

--- src/pu/urls.py
+++ src/pu/urls.py
@@ -36,11 +36,7 @@ urlpatterns = [
     path('ademin/', lv.ademin, name='ademin'),
     path('mypage/', lv.my_page, name='mypage'),
     path('ads/', include('ads.urls'), name='Advertisements'),
-    path('', av.advertisements_view, name='ads_view'),
-    path('<int:pk>/', av.show_specific_ad, name='specific_ad'),
-    path('new/', av.create_ad, name='create_ad'),
-    path('<int:pk>/edit/', av.edit_ad, name='edit_ad'),
-    path('<int:pk>/delete/', av.delete_ad, name='delete_ad'),
+    path('user/', include('users.urls'), name='user')
 ]
 
 # For displaying imagefields
