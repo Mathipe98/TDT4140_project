30c9450de9e77cb3769a750aef005040aec32999
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-03-14 17:33:42 +0100

    Added messaging form in threads
    Fixed several bugs and naming inconcistencies
    Added helper methods to reduce code length and complexity
    Added form to messages in threads

--- src/contact/views.py
+++ src/contact/views.py
@@ -13,15 +13,42 @@ from django.db.models import Q
 
 
 def authenticate_user(request):
+    """ HELPER METHOD """
     user = request.user
     if not user.is_authenticated:
         return redirect('home')
     return user
 
 
+def determine_users_from_thread_id(user, thread_id):
+    """ HELPER METHOD """
+    thread = Thread.objects.get(pk=thread_id)
+    user_from = user
+    if user == thread.user1:
+        user_to = thread.user2
+    elif user == thread.user2:
+        user_to = thread.user1
+    else:
+        return redirect("home")
+    return user_from, user_to, thread
+
+
+def save_msg_form(form, current_thread, user_to, user_from):
+    """ HELPER METHOD """
+    if form.is_valid():
+        messages = form.save(commit=False)
+        messages.thread = current_thread
+        messages.sentto = user_to
+        messages.sentfrom = user_from
+        messages.publish()
+        messages.save()
+
+
 def create_conversation(request, pk):
     userFrom = authenticate_user(request)
     userTo = get_object_or_404(Users, pk=pk)
+    if userFrom == userTo:  # Stops you from sending messages to yourself
+        return redirect("home")
     if (userFrom.userid < pk):
         user1 = userFrom
         user2 = userTo
@@ -34,31 +61,17 @@ def create_conversation(request, pk):
 
 
 def view_conversation(request, pk):
-    thread = Thread.objects.get(pk=pk)
     user = authenticate_user(request)
-    userFrom = user
-    if (user == thread.user1):
-        userTo = thread.user2
-    elif (user == thread.user2):
-        userTo = thread.user1
-    else:
-        return redirect("home")
-    if userFrom == userTo:  # Stops you from sending messages to yourself
-        return redirect("home")
-    test = "User 1: " + str(userFrom.userid) + " User 2: " + str(userTo.userid)
+    user_from, user_to, thread = determine_users_from_thread_id(user, pk)
+    test = "User 1: " + str(user_from.userid) + " User 2: " + str(user_to.userid)
     # Thread.objects.get_or_create(user1=user1,user2=user2)
     # threadUser = Thread.objects.get(user1=user1,user2=user2)
     # test = "User 1: " + str(user1.userid) + " User 2: " + str(user2.userid) + " " + str(threadUser)
     if request.method == "POST":
         form = MessageForm(request.POST)
         if form.is_valid():
-            messageS = form.save(commit=False)
-            messageS.thread = thread
-            messageS.sentto = userTo
-            messageS.sentfrom = userFrom
-            messageS.publish()
-            messageS.save()
-            return redirect("contact", userTo.userid)
+            save_msg_form(form, thread, user_to, user_from)
+            return redirect('contact_user', user_to.userid)
     else:
         form = MessageForm
     return render(request, "sellyoshit/contact.html", {'test': test, 'form': form})
@@ -76,6 +89,7 @@ def thread_view(request, thread_id=-1):
     else:
         current_thread = messages.filter(thread=thread_id).latest('thread_id').thread
         # Gets latest message form requested thread
+
     for message in messages:
         parent_thread = message.thread
         #  Finds the thread that the message belongs to by using the foreign key in message
@@ -83,9 +97,19 @@ def thread_view(request, thread_id=-1):
             threads.append(parent_thread)
         if parent_thread == current_thread:
             current_messages.append(message)  # The message is part of the thread we are looking for
+
+    user_from, user_to, thread = determine_users_from_thread_id(user, current_thread.pk)
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            save_msg_form(form, thread, user_to, user_from)
+            return redirect('view-threads', thread_id)
+    else:
+        form = MessageForm
     return render(request,
                   'message_view.html',
                   {'threads': threads,
                    'user': user,
                    'current_thread': current_thread,
-                   'current_messages': current_messages})
+                   'current_messages': current_messages,
+                   'form': form})
