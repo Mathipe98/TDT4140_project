84be60a5b868dc1977587693cece6d6f5a8ee765
SHIFT-MathiasP <56968311+SHIFT-MathiasP@users.noreply.github.com>
2020-03-18 00:07:26 +0100

    Statistics side vil vises og kun være tilgjengelig for admin
    Det vil komme opp en knapp, når man er innlogget som admin, som redirecter til nettsiden med statistikk. I tillegg, hvis man skriver inn linken til statistikk-siden som en annen bruker (eller som ingen bruker), så blir man redirected til hjemmesiden

--- src/stats/views.py
+++ src/stats/views.py
@@ -4,7 +4,13 @@ from django.core import serializers
 from ads.models import Advertisement
 
 def dashboard_with_pivot(request):
-    return render(request, 'stats/dashboard_with_pivot.html', {})
+    user = request.user
+    if not user.is_authenticated:
+        return redirect('home')
+    elif not user.admin:
+        return redirect('home')
+    else:
+        return render(request, 'stats/dashboard_with_pivot.html', {})
 
 def pivot_data(request):
     dataset = Advertisement.objects.all()
