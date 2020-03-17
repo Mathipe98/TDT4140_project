5419f1af850798647d4909f659608364dc208864
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-03-17 14:39:49 +0100

    Fixed login bug in view test
    Cannot save the password directly in the object, it must be set via a method.
    See https://stackoverflow.com/questions/2619102/djangos-self-client-login-does-not-work-in-unit-tests
    for explanation

--- src/contact/tests/test_views.py
+++ src/contact/tests/test_views.py
@@ -10,14 +10,26 @@ class TestConversation(TestCase):
     def setUp(self):
         self.user1 = Users.objects.create(username="Test", password="tester1234", email="test@test.com",
                                           firstname="Test", lastname="Test", admin=0, blocked=0)
+        self.user1.set_password("tester1234")  # Must be set through method, if not it will save it as a generated hash
+        self.user1.save()
+
         self.user2 = Users.objects.create(username="Test2", password="tester1234", email="test2@test.com",
                                           firstname="Test2", lastname="Test2", admin=0, blocked=0)
+        self.user2.set_password("tester1234")  # Must be set through method, if not it will save it as a generated hash
+        self.user2.save()
         self.client = Client()
 
-        self.client.login(username=self.user1.username, password=self.user1.password)
+        self.client.login(username=self.user1.username, password="tester1234")
 
     def test_create_conversation(self):
+        url = reverse("contact_user", args=[2])
+        response_redirect = self.client.get(url, follow=False)
+        self.assertEqual(response_redirect.url, 'messages/message/1')
+        response = self.client.get(response_redirect)
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_conversation_with_yourself(self):
         url = reverse("contact_user", args=[1])
 
-        response = self.client.get(url, follow=True)
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get(url, follow=False)
+        self.assertEqual(response.url, '/')
