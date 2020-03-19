2a5ecbe5bb00b0ae5a30e661cb0a2d6ecda9b174
Mathipe98 <56786723+Mathipe98@users.noreply.github.com>
2020-03-19 12:59:00 +0100

    Revert "Added average rating and rating count to user profile"
    This reverts commit 41e73f6dc5f7ae073c0c5db44e1cd0918891db93.

--- src/users/views.py
+++ src/users/views.py
@@ -1,16 +1,8 @@
-from django.db.models import Avg
 from django.shortcuts import render, get_object_or_404
 
-from contact.models import Ratings
 from users.models import Users
 
 
 def show_user_profile(request, pk):
     user = get_object_or_404(Users.objects.filter(pk=pk))
-    rating_count = Ratings.objects.filter(rated=user).count()
-    rating_avg = Ratings.objects.filter(rated=user).aggregate(Avg('score'))
-    print(rating_avg)
-    return render(request, 'user_profile.html', {'rating_count':rating_count,
-                                                 'rating_avg':rating_avg,
-                                                 'user': user,
-                                                 "request": request})
+    return render(request, 'user_profile.html', {'user': user, "request": request})
