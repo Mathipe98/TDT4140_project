b48f06decca6b79dbaf1d197e2c419741d33867f
matsorens <mws@sf-nett.no>
2020-03-19 19:33:55 +0100

    changed tests to work with the new field in advertisement models; category

--- src/ads/tests.py
+++ src/ads/tests.py
@@ -1,15 +1,22 @@
 from django.test import SimpleTestCase
 from ads.forms import AdvertisementForm
+from .models import Category
+from django.test import TestCase
 
 
-class TestForms(SimpleTestCase):
+class TestForms(TestCase):
     """This class tests the input fields in the form that is used when creating or editing a new ad."""
+
+    def setUp(self):
+        """Making a category for testing"""
+        Category.objects.create(name='TestCategory')
 
     def test_ads_form_valid_data(self):
         form = AdvertisementForm(data={
             'product_name': 'Car',
             'product_description': 'I want to sell my Chevrolet 1980 model',
-            'price': 1000000
+            'price': 1000000,
+            'category': Category.objects.get(name='TestCategory')
         })
 
         self.assertTrue(form.is_valid())
@@ -18,7 +25,8 @@ class TestForms(SimpleTestCase):
         form = AdvertisementForm(data={
             'product_name': 'Car',
             'product_description': 'I want to sell my Chevrolet 1980 model',
-            'price': -1000
+            'price': -1000,
+            'category': Category.objects.get(name='TestCategory')
         })
 
         self.assertFalse(form.is_valid())
@@ -27,6 +35,7 @@ class TestForms(SimpleTestCase):
         form = AdvertisementForm(data={
             'product_name': 'Car',
             'product_description': 'I want to sell my Chevrolet 1980 model',
+            'category': Category.objects.get(name='TestCategory')
         })
 
         self.assertFalse(form.is_valid())
@@ -34,7 +43,8 @@ class TestForms(SimpleTestCase):
     def test_ads_form_no_product_name(self):
         form = AdvertisementForm(data={
             'product_description': 'I want to sell my Chevrolet 1980 model',
-            'price': 10
+            'price': 10,
+            'category': Category.objects.get(name='TestCategory')
         })
 
         self.assertFalse(form.is_valid())
@@ -42,7 +52,8 @@ class TestForms(SimpleTestCase):
     def test_ads_form_no_product_description(self):
         form = AdvertisementForm(data={
             'product_name': 'Car',
-            'price': 10
+            'price': 10,
+            'category': Category.objects.get(name='TestCategory')
         })
 
         self.assertFalse(form.is_valid())
--- src/sellyoshit/urls.py
+++ src/sellyoshit/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-
+from .views import products, ad
 
 app_name = 'shop'
+
+"""These urlpatterns are used for testing purposes. Deleting them will cause errors in the test."""
 urlpatterns = [
-    #path('products/', products, name='products'),
-    #path('details/<int:pk>', ad, name='details'),
-
-
+    path('products/', products, name='products'),
+    path('details/<int:pk>', ad, name='details'),
 ]
