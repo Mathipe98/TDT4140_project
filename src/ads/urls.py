686b8f17fcdd2c8ac70da4823c5ddfd20df56254
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-03-22 20:23:35 +0100

    Updated ads urls and deleted unnecessary files
    Removed redundant urls in the pu/urls file.
    Deleted unused html and css files

--- src/ads/urls.py
+++ src/ads/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views as av
 
 urlpatterns = [
-    path('', av.advertisements_view, name='ads_view'),
     path('<int:pk>/', av.show_specific_ad, name='specific_ad'),
     path('new/', av.create_ad, name='create_ad'),
     path('<int:pk>/edit/', av.edit_ad, name='edit_ad'),
     path('<int:pk>/delete/', av.delete_ad, name='delete_ad'),
-    path('<int:pk>/update/', av.sold_ad, name='sold_ad')
+    path('<int:pk>/update/', av.sell_ad, name='sold_ad')
 ]
 
