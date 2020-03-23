91af07aded7c628a6b9e0023db11185f38db7fef
simbli <simon.blindheim@ntnu.no>
2020-03-23 14:45:32 +0100

    Add integration and unit test functions

---
+++ src/stats/tests.py
@@ -0,0 +1,81 @@
+""" Integration and unit testing module using Django test objects
+
+    Classes:
+        TestUsersAttributes
+        TestAdvertisementAttributes
+        TestStatisticsPageInput
+"""
+
+from datetime import datetime
+
+from django.test import SimpleTestCase
+
+from ads.models import Advertisement
+from stats.views import statistics_context
+from users.models import Users
+
+
+class TestUsersAttributes(SimpleTestCase):
+    """ This class tests if the Users class has the correct attributes """
+
+    def test_if_class_has_objects_attribute(self):
+        self.assertTrue(hasattr(Users, 'objects'))
+
+    def test_if_class_objects_has_all_method(self):
+        self.assertTrue(callable(getattr(Users.objects, 'all', None)))
+
+    def test_if_all_objects_has_count_method(self):
+        self.assertTrue(callable(getattr(Users.objects.all(), 'count', None)))
+
+    def test_if_count_returns_int(self):
+        self.assertIsInstance(Users.objects.all().count(), int)
+
+
+class TestAdvertisementAttributes(SimpleTestCase):
+    """ This class tests if the Advertisement class has the correct attributes """
+
+    def test_if_class_has_objects_attribute(self):
+        self.assertTrue(hasattr(Advertisement, 'objects'))
+
+    def test_if_class_objects_has_all_method(self):
+        self.assertTrue(callable(getattr(Advertisement.objects, 'all', None)))
+
+    def test_if_all_objects_has_count_method(self):
+        self.assertTrue(callable(getattr(Advertisement.objects.all(), 'count', None)))
+
+    def test_if_count_returns_int(self):
+        self.assertIsInstance(Advertisement.objects.all().count(), int)
+
+    def test_if_class_has_sold_attribute(self):
+        self.assertTrue(hasattr(Advertisement, 'sold'))
+
+    def test_if_sold_attribute_is_bool(self):
+        self.assertIsInstance(Advertisement.sold, bool)
+
+    def test_if_class_has_sold_date_attribute(self):
+        self.assertTrue(hasattr(Advertisement, 'sold_date'))
+
+    def test_if_sold_date_attribute_is_datetime(self):
+        self.assertIsInstance(Advertisement.sold_date, datetime)
+
+    def test_if_class_has_published_date_attribute(self):
+        self.assertTrue(hasattr(Advertisement, 'published_date'))
+
+    def test_if_published_date_attribute_is_datetime(self):
+        self.assertIsInstance(Advertisement.published_date, datetime)
+
+
+class TestStatisticsPageInput(SimpleTestCase):
+    """ This class tests the validity of the context input fields shown in the statistics page """
+
+    def test_users_count_validity(self):
+        self.assertIsInstance(statistics_context()['users_count'], int)
+
+    def test_advertisement_count_validity(self):
+        self.assertIsInstance(statistics_context()['advertisements_count'], int)
+
+    def test_sold_items_count_validity(self):
+        self.assertIsInstance(statistics_context()['sold_items_count'], int)
+
+    def test_average_sell_time_validity(self):
+        self.assertIsInstance(statistics_context()['average_sell_time'], str)
