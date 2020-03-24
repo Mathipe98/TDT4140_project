ca5f113b975c444b65dbd0b07c97095ccdcfffba
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-03-24 16:14:55 +0100

    Documented users URLs and views

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,6 +1,14 @@
+"""The urls that are valid for /ads/ and the views they are associated with
+
+    Variables
+        urlpatterns: List
+            List of the valid urls and their corresponding views
+"""
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('<int:pk>/', views.show_user_profile, name='user_profile')
+    # <int:pk> means that any integer passed into the url will be passed into the view's pk parameter
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,13 @@
+"""
+Display the user profile of a specific user
+
+Functions:
+
+    show_user_profile(request, pk): render
+        Returns a render with the specific information of a requested user
+
+"""
+
 from django.db.models import Avg
 from django.shortcuts import render, get_object_or_404
 
@@ -6,12 +16,20 @@ from users.models import Users
 
 
 def show_user_profile(request, pk):
+    """
+    Finds a specific user via primary key. Returns a render with the specific information of that user.
+    :param request: A django HttpRequest object. The session of the user.
+    :param pk: The primary key of the requested user
+    :return: render
+                a render of the user_profile.html page with the specific information for that user
+    """
     user = get_object_or_404(Users.objects.filter(pk=pk))
+    # Either gives the request user, or a 404 page if the user is not found
 
-    # Gets amount of ratings and average score for the user
     rating_count = Ratings.objects.filter(rated=user).count()
+    # Gets amount of ratings and average score for the user
     rating_avg = Ratings.objects.filter(rated=user).aggregate(Avg('score'))
-    return render(request, 'user_profile.html', {'rating_count':rating_count,
-                                                 'rating_avg':rating_avg,
+    return render(request, 'user_profile.html', {'rating_count': rating_count,
+                                                 'rating_avg': rating_avg,
                                                  'user': user,
                                                  "request": request})
