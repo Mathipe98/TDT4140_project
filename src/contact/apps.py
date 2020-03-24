5996a8c82435ad511a799db57f5efb832075b797
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-03-24 15:08:18 +0100

    Added docstrings to app files

--- src/contact/apps.py
+++ src/contact/apps.py
@@ -1,4 +1,6 @@
-# TODO: Add docstring
+"""
+    Defines the name of the Django app found in this folder
+"""
 
 from django.apps import AppConfig
 
--- src/login/apps.py
+++ src/login/apps.py
@@ -1,3 +1,7 @@
+"""
+    Defines the name of the Django app found in this folder
+"""
+
 from django.apps import AppConfig
 
 
