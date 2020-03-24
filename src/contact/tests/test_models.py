59c2b3165e2c8d16a66a193463f5391c71054cf7
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-03-24 16:08:46 +0100

    Documented tests in contact
    Also added TODOs for things that need to be assessed by others

--- src/contact/tests/test_models.py
+++ src/contact/tests/test_models.py
@@ -1,3 +1,12 @@
+"""
+    Tests that the models in contact function as intended.
+
+    Classes
+        ThreadTestCase: TestCase
+            Tests that the Thread model found in contact/models.py work as intended together with the associated models.
+    
+"""
+
 from django.test import TestCase
 from contact.models import Thread, Messages
 from users.models import Users
@@ -5,8 +14,20 @@ from django.utils import timezone
 
 
 class ThreadTestCase(TestCase):
+    """
+    Class testing that the Thread model found in contact/models.py work as intended together with the associated models.
+
+    Functions
+        setUp(self) : None
+            Set up that occurs before each test
+        test_get_thread_id(self): None
+            Tests that the thread id is not 0 or negative
+        test_publish(self): None
+            Tests that the publish method in Messages works as intended
+    """
 
     def setUp(self):
+        """Creates a setup for which all the tests use. Returns None"""
         self.user1 = Users.objects.create(username="Test", password="tester1234", email="test@test.com",
                                           firstname="Test", lastname="Test", admin=0, blocked=0)
         self.user2 = Users.objects.create(username="Test2", password="tester1234", email="test2@test.com",
@@ -20,9 +41,11 @@ class ThreadTestCase(TestCase):
         )
 
     def test_get_threadid(self):
+        """Tests that the thread id cannot be 0 or negative. Returns None"""
         self.assertGreater(self.thread.get_threadid(), 0)
 
-    def test_publish(self):
+    def test_publish(self):  # TODO: Move to another class?
+        """Tests the publish method found in Messages. Returns None"""
         self.message.publish()
         self.assertGreater(self.message.sent.microsecond, timezone.now().microsecond - 2000)  # Takes some mS to execute
 
--- src/contact/tests/test_views.py
+++ src/contact/tests/test_views.py
@@ -1,78 +1,156 @@
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
+"""
+    Tests the functionality of the different views found in contact/views.py
+
+    Functions:
+        create_user(user_nr): Users
+            Helper method for creating a user
+        create_thread(user1, user2): Thread
+            Helper method for creating a thread
+        create_message(thread, sentto, sentfrom): Messages
+            Helper method for creating a message
+
+    Classes:
+        SetUp(TestCase)
+            Super class. Runs the setUp that is equal for most of the sub-classes.
+        TestConversation(SetUp)
+            Tests that the create_conversation method in views works as intended
+        TestDetermineUsers(SetUp)
+            Tests that the helper method determine_users_from_thread in views works as intended
+        TestViewThread(SetUp)
+            Tests that the view_threads method in views works as intended
+
+"""
+
 from django.test import TestCase, Client
 from django.urls import reverse
 
-from contact.forms import MessageForm
-from contact.models import Thread, Messages, Ratings
-from contact.views import create_conversation, thread_view, determine_users_from_thread_id, save_msg_form
+from contact.models import Thread, Messages
+from contact.views import determine_users_from_thread_id
 from users.models import Users
 
 
 def create_user(user_nr):
-    """Helper method"""
+    """
+    Creates a user with the specified number in the username. Returns Users object
+
+    :param user_nr: int . The # of the user's name.
+    :return Users object
+    """
     return Users.objects.create(username="Test" + str(user_nr), password="tester1234", email="test@test.com",
                                 firstname="Test", lastname="Test", admin=0, blocked=0)
 
 
 def create_thread(user1, user2):
-    """Helper method"""
+    """
+    Creates a thread between two specified users. Returns Thread object
+
+    :param user1: int. The primary key of the first user
+    :param user2: int. The primary key of the second user.
+    :return Thread object
+    """
     return Thread.objects.create(user1_id=user1, user2_id=user2)
 
 
-def create_message(thread, sentto, sentfrom):
-    """Helper method"""
-    return Messages.objects.create(thread_id=thread.pk, sentto=sentto, sentfrom=sentfrom)
+def create_message(thread, sent_to, sent_from):
+    """
+    Creates a message between two specified users. Returns Messages object
+
+    :param thread: Thread. The Thread object between the two users
+    :param sent_to: int. The primary key of the receiving user
+    :param sent_from: int. The primary key of the sending user
+    :return Messages object
+    """
+    return Messages.objects.create(thread_id=thread.pk, sentto=sent_to, sentfrom=sent_from)
 
 
 class SetUp(TestCase):
+    """
+    Super class for the other test classes. Subclass of TestCase. Runs setUp that is equal for most of the sub-classes.
+
+    Functions
+        setUp: None
+            Runs setup that occurs before each test
+
+    Attributes
+        PASSWORD: string
+            common password used for all created users
+        user1: Users
+            first user in each test
+        user2: Users
+            second user in each test
+        client: Client
+            the client used to request views
+    """
     PASSWORD = "tester1234"
 
     def setUp(self):
-        """Set up for each test. Super class for all the remaining test classes."""
+        """Sets up for each test"""
         self.user1 = create_user(1)
         self.user1.set_password(self.PASSWORD)  # Must be set through method, if not it will save it as a generated hash
-        self.user1.save()
+        self.user1.save()  # Saves the updated model
 
         self.user2 = create_user(2)
         self.user2.set_password(self.PASSWORD)  # Must be set through method, if not it will save it as a generated hash
-        self.user2.save()
+        self.user2.save()  # Saves the updated model
+
         self.client = Client()
         self.client.login(username=self.user1.username, password=self.PASSWORD)
 
 
 class TestConversation(SetUp):
+    """
+    Subclass of SetUp. Tests that the create_conversation method in views works as intended
+
+    Functions
+        setUp: None
+            Runs setup that occurs before each test
+        test_create_conversation: None
+            Tests creation of conversations via views
+        test_create_convo_with_yourself: None
+            Tests whether you can create a convo with yourself
+        test_create_convo_with_nonexistant_user: None
+            Tests whether you can create a convo with someone who does not exist
+        test_create_convo_not_logged_in: None
+            Tests whether you can create a convo when you are not logged in
+
+    """
 
     def setUp(self):
+        """Runs setup that occurs before each test. Returns None."""
         super().setUp()
 
     def test_create_conversation(self):
-        url = reverse("contact_user", args=[self.user2.pk])
+        """Tests creation of conversations via views. Returns None."""
+        url = reverse("contact_user", args=[self.user2.pk])  # Requests the url of contact_user with pk = user2.pk
         self.assertEqual(url, '/messages/contact/2/')
         response_redirect = self.client.get(url, follow=False)
+        # Follow = false means it won't be redirected immediately
         self.assertEqual(response_redirect.url, '/messages/messages/1')
         self.assertEqual(response_redirect.status_code, 302)
-        response = self.client.get(response_redirect.url)
+        response = self.client.get(response_redirect.url)  # Follows the redirect url now
         self.assertEqual(response.status_code, 200)
 
     def test_create_convo_with_yourself(self):
+        """Tests whether you can create a convo with yourself. Returns None."""
         url = reverse("contact_user", args=[self.user1.pk])  # Tries to contact himself
         response_redirect = self.client.get(url, follow=False)
         self.assertEqual(response_redirect.url, '/')  # The home page url
 
     def test_create_convo_with_nonexistant_user(self):
+        """Tests whether you can create a convo with someone who does not exist. Returns None."""
         url = reverse("contact_user", args=[3])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     def test_create_convo_not_logged_in(self):
+        """Tests whether you can create a convo when you are not logged in. Returns None."""
         self.client.logout()
         url = reverse("contact_user", args=[self.user1.pk])
         response = self.client.get(url)
         self.assertEqual(response.url, '/')
 
 
-class TestMessages(SetUp):
+class TestMessages(SetUp):  # TODO: Remove?
 
     def setUp(self):
         super().setUp()
@@ -80,40 +158,85 @@ class TestMessages(SetUp):
 
 
 class TestDetermineUsers(SetUp):
+    """
+    Tests that the helper method determine_users_from_thread in views works as intended
+
+    Functions
+        setUp: None
+            Runs setup that occurs before each test
+        test_valid: None
+            Tests whether the function works as intended with valid input
+        test_user_not_in_thread: None
+            Tests whether the function acts correctly when given a user who's not in the given thread
+
+    Attributes
+        thread: Thread
+            A thread between user1 and user2
+    """
 
     def setUp(self):
+        """Runs setup that occurs before each test. Returns None."""
         super().setUp()
         self.thread = create_thread(self.user1.pk, self.user2.pk)
 
     def test_valid(self):
+        """Tests whether the function works as intended with valid input. Returns None."""
         result_user_from, result_user_to, result_thread = determine_users_from_thread_id(self.user1, 1)
         self.assertEqual(result_user_from, self.user1)
         self.assertEqual(result_user_to, self.user2)
         self.assertEqual(result_thread.pk, self.thread.pk)
 
     def test_user_not_in_thread(self):
-        self.user3 = Users.objects.create(username="Test3", password="tester1234", email="test@test.com",
-                                          firstname="Test", lastname="Test", admin=0, blocked=0)
-        result_user_from, result_user_to, result_thread = determine_users_from_thread_id(self.user3, 1)
+        """Tests whether the function acts correctly when given a user who's not in the given thread. Returns None."""
+        user3 = Users.objects.create(username="Test3", password="tester1234", email="test@test.com",
+                                     firstname="Test", lastname="Test", admin=0, blocked=0)
+        result_user_from, result_user_to, result_thread = determine_users_from_thread_id(user3, 1)
         self.assertEqual(result_user_from, None)
 
 
 class TestViewThread(SetUp):
+    """
+    Tests that the thread_view view functions as intended
+
+    Functions
+        setUp: None
+            Runs setup that occurs before each test
+        test_logged_out: None
+            Tests that the user cannot view a thread when logged out, and is redirected to home
+        test_no_threads_latest: None
+            Tests that a user who is not part of any threads should be given an error page
+        test_nonexistant_thread_request: None
+            Tests that requesting a nonexistant thread yields and error page
+        test_valid_thread: None
+            Tests that creating a thread with valid information works
+
+
+    Attributes
+        user3: Users
+            A third user
+        thread1: Thread
+            A thread between user1 and user2
+        url: string
+            The reversed url for view-threads
+    """
 
     def setUp(self):
+        """Runs setup that occurs before each test. Returns None."""
         super().setUp()
         self.user3 = create_user(3)
         self.thread1 = create_thread(self.user1.pk, self.user2.pk)
         self.url = reverse('view-threads')
 
     def test_logged_out(self):
+        """Tests that the user cannot view a thread when logged out. Returns None"""
         self.client.logout()
-        response = self.client.get(self.url, follow=False)
+        response = self.client.get(self.url, follow=False)  # Follow = false means it won't be redirected immediately
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, '/')
         # Redirect to home page
 
     def test_no_threads_latest(self):
+        """Tests that a user who is not part of any threads should be given an error page. Returns None"""
         self.user100 = create_user(100)
         self.client.get(self.url, follow=False)
         response = self.client.get(self.url)
@@ -122,16 +245,16 @@ class TestViewThread(SetUp):
         self.assertIn('You have not created any conversations yet', str(response.content))
 
     def test_nonexistant_thread_request(self):
-        url = reverse('view-threads', args=[2])
+        """Tests that requesting a nonexistant thread yields and error page. Returns None"""
+        url = reverse('view-threads', args=[2])  # Sends in 2 as a url argument to the view-threads url
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         # Should return 200 OK but with different content than a valid thread
         self.assertIn('There is no thread with this ID currently existing. Create a convo first', str(response.content))
 
     def test_valid_thread(self):
+        """Tests that creating a thread with valid information works. Returns None"""
         self.msg = create_message(self.thread1, self.user1, self.user2)
         response = self.client.get(self.url)  # Should not redirect anywhere else
         self.assertEqual(response.status_code, 200)
         self.assertIn('Your conversation with', str(response.content))
-
-
